97819afe38ea909c52699b683c663477bea6f330
Edward <koo22952@gmail.com>
2019-08-16 00:09:22 +0800

    設定 全域SCSS

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VeeValidate from 'vee-validate'
+import '@/assets/scss/index.scss'
 
 Vue.use(BootstrapVue, VeeValidate)
 Vue.config.productionTip = false
